fe3b4a2ca45b10a8346130492dbd1b7a94a74735
Bobur Abdusalomov <babdusalom72@gmail.com>
2024-08-14 14:53:33 +0500

    doneee

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   Person person=new Person();
+
+
+   System.out.println(person.name+person.surname+person.country);
+
+    }
+}
+ 
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+     String name;
+     String surname;
+String country;
+   
+
+
+    public Person() {
+        country="uz";
+        
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+
+        this.surname = surname;
+        System.out.println("Person");
+    }
+}
